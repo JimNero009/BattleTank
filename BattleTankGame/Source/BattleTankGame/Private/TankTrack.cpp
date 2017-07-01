3e5a9d1b40601c9a0318886f568bb9c1d83829e5
JDC_Windows <james.cockburn.09@icloud.com>
2017-07-01 13:49:00 +0100

    Bound input to throttle values

---
+++ BattleTankGame/Source/BattleTankGame/Private/TankTrack.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle) {
+	Throttle = FMath::Clamp<float>(Throttle, -1, 1);
+	UE_LOG(LogTemp, Warning, TEXT("%s has throttle of %f"), *GetName(), Throttle);
+}
+
+
